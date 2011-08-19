752be44dbb03b8a63934cd4264b1966518e47d59
Dave Artz <dave@artzstudio.com>
2011-08-19 13:54:40 -0400

    Cleaned up ajaxproxy.php.

--- api/ajaxproxy.php
+++ api/ajaxproxy.php
@@ -1,24 +1,24 @@
 <?php
 
     /*
-     * Super simple AJAX Proxy
+     * Super simple AJAX -> JSONP Proxy
      * Like... seriously simple
      */
 
     header( 'content-type: application/javascript; charset=utf-8' );
 
     if ( isset( $_GET['file'] ) && file_exists( $_GET['file'] ) ) {
+		
         // Retrieve dummy JSON data stored as a file in this directory
-
         $data = file_get_contents( './' . $_GET['file'] ); 
 
         // Simulate asynchronicity delay so nobody accidentally assumes synchronicity
         sleep( ( isset( $_GET['delay'] ) ) ? $_GET['delay'] : 1 );
 
     } elseif ( isset( $_GET['url'] ) ) {
+		
         // Proxy JSON from an external URL
         // (If your API serves JSONP, why are you using this, buddy?)
-
         $proxy = curl_init( urldecode( $_GET['url'] ) );
 
         // We want to store the return as a variable, not just output it
@@ -35,5 +35,4 @@
 
     $callback = ( isset( $_GET['callback'] ) ) ? $_GET['callback'] : 'callback';
 
-    echo $callback . '(' . $data . ')';
-?>
+    echo $callback . '(' . $data . ');';
