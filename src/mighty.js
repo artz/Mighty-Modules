80d0220c593f457474ab0be573230420b64e5935
Dave Artz <dave@artzstudio.com>
2011-08-30 16:50:56 -0400

    Exposed mighty.init() to scan the DOM and initialize mighty modules.

--- src/mighty.js
+++ src/mighty.js
@@ -8,6 +8,7 @@
 		// Localize Boot functions we use more than once.
 	var boot = Boot,
 		widget = boot.widget,
+		mighty = boot.mighty,
 		extend = boot.extend,
 		
 		toArray = function( collection ) { 
@@ -17,11 +18,7 @@
 				array[l] = collection[l];
 			}
 			return array;
-		},
-		
-		mightyModules = toArray( document.getElementsByName( global ) ),
-		i = 0, 
-		l = mightyModules.length;
+		};
 
 	// We'll move this to Boot eventually, after 
 	// we ensure all functionality provided by 
@@ -50,60 +47,72 @@
 			return instance;
 		};
 	}
-
-	for (; i < l; i++) {
+	
+	if ( ! mighty ) {
 		
-		// Closure to isolate vars.
-		!function( elem ){ 
+		mighty = boot.mighty = {};
+		mighty.init = function(){
 		
-			// We need to do this so IE6/7 execute things in
-			// the correct order.  Very, very bizarre.
-			boot.defer(function(){
-			
-				if ( elem && elem.nodeName === "A" && ! elem.widget ) {
-					
-					// Remember we turned this into a widget already.
-					elem.widget = 1;
-						
-					// Decide if we want to move this into widget class
-					// or outside of it.
-					var widgetName = elem.className.replace("-", "."),
-						elemParent = elem.parentNode,
-						div,
-							
-						// Do our fancy DOM option extraction here.
-						options = extend( options || {}, boot.data( elem ) );
+			var mightyModules = toArray( document.getElementsByName( global ) ),
+				i = 0, 
+				l = mightyModules.length;
 				
-					// If we have an anchor link placeholder, replace 
-					// it with a <div> so we have a valid container.
-					if ( elem.nodeName === "A" ) {
-		
-						div = document.createElement("div");
-		
-						div.className = elem.className;
-						
-						// These log checks were F'ed up without the
-						// boot.defer wrapper above in IE6/7.
-					//	Boot.log( "My name: " + widgetName );							
-					//	Boot.log( "Setting className: " + div.className );
-		
-						elemParent.insertBefore( div, elem );
-						elemParent.removeChild( elem );				
-						
-						elem = div;
-						
-					}
+			for (; i < l; i++) {
+				
+				// Closure to isolate vars.
+				!function( elem ){ 
+				
+					// We need to do this so IE6/7 execute things in
+					// the correct order.  Very, very bizarre.
+					boot.defer(function(){
 					
-					// Bring in the modules we need.
-					boot.use({ basePath: "../src/", suffix: ".js" }, widgetName, function( source ) {
+						if ( elem && elem.nodeName === "A" && ! elem.widget ) {
+							
+							// Remember we turned this into a widget already.
+							elem.widget = 1;
+								
+							// Decide if we want to move this into widget class
+							// or outside of it.
+							var widgetName = elem.className.replace("-", "."),
+								elemParent = elem.parentNode,
+								div,
+									
+								// Do our fancy DOM option extraction here.
+								options = extend( options || {}, boot.data( elem ) );
 						
-						// Make this guy a widget.
-						var myWidget = widget( widgetName, elem, options );
-						
+							// If we have an anchor link placeholder, replace 
+							// it with a <div> so we have a valid container.
+							if ( elem.nodeName === "A" ) {
+				
+								div = document.createElement("div");
+				
+								div.className = elem.className;
+								
+								// These log checks were F'ed up without the
+								// boot.defer wrapper above in IE6/7.
+							//	Boot.log( "My name: " + widgetName );							
+							//	Boot.log( "Setting className: " + div.className );
+				
+								elemParent.insertBefore( div, elem );
+								elemParent.removeChild( elem );				
+								
+								elem = div;
+								
+							}
+							
+							// Bring in the modules we need.
+							boot.use({ basePath: "../src/", suffix: ".js" }, widgetName, function( source ) {
+								
+								// Make this guy a widget.
+								var myWidget = widget( widgetName, elem, options );
+								
+							});
+						}
 					});
-				}
-			});
-		}( mightyModules[i] );
+				}( mightyModules[i] );
+			}
+		};
 	}
+	mighty.init();
 	
 }("mighty", this, document );
