2c38495d1aa39825f6710209a1285be56166d909
Ramesh Kumar <rameshk2k2@gmail.com>
2011-10-04 10:02:23 -0400

    Updates to Most Watched Videos widget

--- src/mighty/mostwatchedvideos/mighty.mostwatchedvideos.js
+++ src/mighty/mostwatchedvideos/mighty.mostwatchedvideos.js
@@ -1,6 +1,7 @@
 Mighty.define(["mighty.core"], function( core ){
 	
-	var adDivCounter = 0;
+	var adDivCounter = 0,
+		which = 0;
 	
 	return {			
 		
@@ -13,32 +14,33 @@ Mighty.define(["mighty.core"], function( core ){
 		// Set up the widget
 		_create: function () {
 			
-			var self = this,
-				options = self.options,
-				element = self.element,
-				ui = self.ui;
-				images = core.query('.mighty-videos-li img', element);
-				totalCount = images.length;
+					var self = this,
+						options = self.options,
+						element = self.element,
+						ui = self.ui;
+						images = core.query('.mighty-videos-li img', element);
+						totalCount = images.length;
+						totalCount = totalCount-1; // Doing this, coz the index starts with 0 and not 1
+						
+						ui.videoSlide = document.createElement( 'div' );
+						ui.videoSlide.className = "mighty-vid";
+						element.insertBefore( ui.videoSlide, element.lastChild );
+										
+						// Ad related markup goes here
+						adDivId = 0;
+						adDivName = "mighty-mostwatchedvideos-ad";
+						AdId = adDivName + ( adDivCounter++ );
+									
+						
+					core.getCSS("../src/mighty/mostwatchedvideos/mighty.mostwatchedvideos.css");
+					
+					if( options.ads == 1 ){
+						self._adInclude();
+					}
 				
-				ui.videoSlide = document.createElement( 'div' );
-				ui.videoSlide.className = "mighty-vid";
-				element.insertBefore( ui.videoSlide, element.lastChild );
-								
-				
-				adDivId = 0;
-				adDivName = "mighty-mostwatchedvideos-ad";
-				AdId = adDivName + ( adDivCounter++ );
-							
-				
-			core.getCSS("../src/mighty/mostwatchedvideos/mighty.mostwatchedvideos.css");
-			
-			if( options.ads == 1 ){
-				self._adInclude();
-			}
-		
-			self._loadSlide(0);
-			
-			self._bindevents(1);			
+					self._loadSlide(0);
+					
+					self._bindEvents();			
 		},
 		
 		_loadSlide : function (which) {
@@ -55,9 +57,10 @@ Mighty.define(["mighty.core"], function( core ){
 									elm.href = elm.getAttribute('data-href');
 									elm.title = elm.getAttribute('data-title');
 									
-									mightyHTML = '<div class="mighty-mostwatchedvideos-active"><a href="'+elm.href+'" target="_blank"><img src="'+elm.src+'" /></a><p><a href="'+elm.href+'" target="_blank">'+elm.title+'</a></p></div>';
+									mightyHTML = '<div class="mighty-mostwatchedvideos-active"><a href="'+elm.href+'" title="'+elm.title+'" class="mighty-mostwatchedvideos-play">Play</a><a href="'+elm.href+'" target="_blank"><img src="'+elm.src+'" /></a><p><a href="'+elm.href+'" target="_blank">'+elm.title+'</a></p></div>';
 									
 									ui.videoSlide.innerHTML =  mightyHTML;
+									//console.log(which);
 																		
 							}
 							
@@ -67,54 +70,68 @@ Mighty.define(["mighty.core"], function( core ){
 		
 		
 		_adInclude: function () {
-			var self = this,
-				options = self.options,
-				element = self.element,
-				ui = self.ui;				
-			
-			ui.adDiv = document.createElement( 'div' );
-			ui.adDiv.className = "mighty-ad";
-			ui.adDiv.innerHTML = '<div class="footer-links"><a href="http://www.huffingtonpost.com/" class="logo"><img src="../../../images/mostpopular/huffpost.png" width="120" /></a><a href="#" class="link">Get This Widget</a></div><div id="'+ AdId +'" class="advertisement"></div>';
-			
-			element.insertBefore( ui.adDiv, element.lastChild );
-			
-			// Render the ad.
-			if ( window.htmlAdWH ) {
-					htmlAdWH( 808216, 234, 60, "ajax", AdId );
-				}			
+					var self = this,
+						options = self.options,
+						element = self.element,
+						ui = self.ui;				
+					
+					ui.adDiv = document.createElement( 'div' );
+					ui.adDiv.className = "mighty-ad";
+					ui.adDiv.innerHTML = '<div class="footer-links"><a href="http://www.huffingtonpost.com/" class="logo"><img src="../../../images/mostpopular/huffpost.png" width="120" /></a><a href="#" class="link">Get This Widget</a></div><div id="'+ AdId +'" class="advertisement"></div>';
+					
+					element.insertBefore( ui.adDiv, element.lastChild );
+					
+					// Render the ad.
+					if ( window.htmlAdWH ) {
+							htmlAdWH( 808216, 234, 60, "ajax", AdId );
+						}			
 		
 		},
 		
-		_bindevents: function ( which ) {
+		_bindEvents: function () {
 			
-			var self = this,
-				options = self.options,
-				which = which,
-				nextSlideNumber,
-				element = self.element;			
-											
-			core.delegate( element, ".mighty-mostwatchedvideos-controls-next", "click", function( event ){
-				nextSlideNumber = which++;
-				console.log(nextSlideNumber);
-
-				self._loadSlide(nextSlideNumber);
-				
-				event.preventDefault();					
-			});
-			core.delegate(element,'.mighty-mostwatchedvideos-controls-prev','click',function( event ){
-				nextSlideNumber = which--;
-				if (nextSlideNumber == 0){
-					which = totalCount;
-				}
-				console.log(nextSlideNumber);
-				
-				self._loadSlide(nextSlideNumber);
+					var self = this,
+						options = self.options,
+						nextSlideNumber,
+						prevSlideNumber,
+						element = self.element;			
+													
+					core.delegate( element, ".mighty-mostwatchedvideos-controls-next", "click", function( event ){
+						
+							nextSlideNumber = which+1;
+							if (nextSlideNumber >= totalCount){
+								nextSlideNumber = 0;
+							}							
+							self._loadSlide(nextSlideNumber);
+								which = nextSlideNumber;
 								
-				event.preventDefault();	
-			
-			});
+							prevSlideNumber = which-1;
+							console.log(prevSlideNumber);
+							event.preventDefault();		
+							
+					});
+					core.delegate(element,'.mighty-mostwatchedvideos-controls-prev','click',function( event ){
+							//console.log(which);
+							if (which <= 0){
+								which = totalCount;
+							}
+							prevSlideNumber = which--;
+											
+							self._loadSlide(prevSlideNumber);
+							if (prevSlideNumber >= totalCount ){
+								prevSlideNumber = 0;
+							}
+							nextSlideNumber = prevSlideNumber+1;
+							if (nextSlideNumber >= totalCount ){
+								nextSlideNumber = 0;
+							}
+							console.log(nextSlideNumber);							
+											
+							event.preventDefault();	
+					
+					});
 				
-		},
+		}
 
 	};
 	
