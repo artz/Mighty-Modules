3443be8f4cc83928eeceb2510e0ca1cdd47b3d30
Dave Artz <dave@artzstudio.com>
2011-10-04 22:29:20 -0400

    Style updates to factory.

--- factory/js/factory.js
+++ factory/js/factory.js
@@ -8,16 +8,20 @@
 			
 			$factory = $(".factory"),
 			
+			$logoFruit = $(".logo > b"),
+			
 			factoryMakeHTML = $factory.html(), // save original html
 			
 			selectedWidget;
 		
+		// bind widget clicks
 		$(document).delegate( ".widget", "click", function(){
 			$Observer.trigger("select-widget", {
 				widgetName: $(this).data("widget")
 			});
 		});
 		
+		// handle widget selection
 		$Observer.bind("select-widget", function( event, data ) {
 	
 			var widgetName = data.widgetName;
@@ -32,6 +36,20 @@
 			
 			Mighty.init();
 			
+		});
+		
+		// fancy logo stuff
+		$logoFruit.each(function(){
+			$(this)
+				.addClass("logo-color-" + Math.floor(Math.random()*4))
+				.bind("mouseover", function(){
+					this.className = "logo-color-" + Math.floor(Math.random()*4);
+				});
+		});
+		
+		// reset factory
+		$(document).delegate(".logo", "click", function(){
+			$factory.html( factoryMakeHTML );
 		});
 		
 	});
