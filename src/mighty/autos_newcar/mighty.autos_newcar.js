f322ae0bedfd9dc2e7a41317fa6dfb351fd815cd
Dave Artz <dave@artzstudio.com>
2012-05-25 21:26:39 -0400

    Cleaned up Autos New Car module and further attempted to fix IE issue.

--- src/mighty/autos_newcar/mighty.autos_newcar.js
+++ src/mighty/autos_newcar/mighty.autos_newcar.js
@@ -1,118 +1,122 @@
-Mighty.define(["mighty.core"], function( core ){
+/*global Mighty*/
+/*jslint browser: true*/
+Mighty.define(["mighty.core"], function (core) {
+
+    "use strict";
 
     // This saves an HTTP request for the CSS.
     // This module may be a great example of a no JS widget, loading only CSS.
     core.inlineCSS(".mighty-autos_newcar {background: url(" + Mighty.option("basePath") + "mighty/autos_newcar/aol-autos_logo.jpg) no-repeat 230px bottom #fff;border:5px solid #0069b5;clear:both;min-height: 165px;overflow: hidden;padding:22px 15px;width: 300px;}\
-	@font-face{font-family:'BebasNeue';src:url('http://o.aolcdn.com/os/fonts/BebasNeue-webfont.eot');src:url('http://o.aolcdn.com/os/fonts/BebasNeue-webfont.eot?iefix') format('eot'),url('http://o.aolcdn.com/os/fonts/BebasNeue-webfont.woff') format('woff'),url('http://o.aolcdn.com/os/fonts/BebasNeue-webfont.ttf') format('truetype'),url('http://o.aolcdn.com/os/fonts/BebasNeue-webfont.svg#webfontfvFLBU0N') format('svg');font-weight:normal;font-style:normal;}\
-	.mighty-autos h2 {font-family:BebasNeue;font-size: 32px;margin-bottom: 15px;}\
-	.mighty-autos h2 em {color:#2a65b3;}\
-	.mighty-autos label {display: block;margin-left:10px;}\
-	.mighty-autos input[type='text'], .mighty-autos select {\
-		border: 1px solid #ccc;\
-		border-radius: 4px;\
-		-moz-border-radius: 4px;\
-		display: block;\
-		float: left;\
-		font-size: 10px;\
-		margin: 5px 0;\
-		padding: 0 8px;\
-	}\
-	.mighty-autos input[type='submit'] {\
-		background-color: #b2b2b2;\
-		border-radius: 3px;\
-		-moz-border-radius: 3px;\
-		color: #fff;\
-		float: right;\
-		font-size: 12px;\
-		font-weight: bold;\
-		margin: 5px 0;\
-		padding: 2px 14px 1px 14px;\
-	}\
-	.mighty-autos input[type='submit']:hover {\
-		background-color: #2a65b3;\
-		cursor: pointer;\
-	}\
-	.mighty-autos input[type='text'] {width: 100px;}\
-	.mighty-autos select {\
-		background: transparent;\
-		height: 24px;\
-		width: 100%;\
-	}");
+    @font-face{font-family:'BebasNeue';src:url('http://o.aolcdn.com/os/fonts/BebasNeue-webfont.eot');src:url('http://o.aolcdn.com/os/fonts/BebasNeue-webfont.eot?iefix') format('eot'),url('http://o.aolcdn.com/os/fonts/BebasNeue-webfont.woff') format('woff'),url('http://o.aolcdn.com/os/fonts/BebasNeue-webfont.ttf') format('truetype'),url('http://o.aolcdn.com/os/fonts/BebasNeue-webfont.svg#webfontfvFLBU0N') format('svg');font-weight:normal;font-style:normal;}\
+    .mighty-autos h2 {font-family:BebasNeue;font-size: 32px;margin-bottom: 15px;}\
+    .mighty-autos h2 em {color:#2a65b3;}\
+    .mighty-autos label {display: block;margin-left:10px;}\
+    .mighty-autos input[type='text'], .mighty-autos select {\
+        border: 1px solid #ccc;\
+        border-radius: 4px;\
+        -moz-border-radius: 4px;\
+        display: block;\
+        float: left;\
+        font-size: 10px;\
+        margin: 5px 0;\
+        padding: 0 8px;\
+    }\
+    .mighty-autos input[type='submit'] {\
+        background-color: #b2b2b2;\
+        border-radius: 3px;\
+        -moz-border-radius: 3px;\
+        color: #fff;\
+        float: right;\
+        font-size: 12px;\
+        font-weight: bold;\
+        margin: 5px 0;\
+        padding: 2px 14px 1px 14px;\
+    }\
+    .mighty-autos input[type='submit']:hover {\
+        background-color: #2a65b3;\
+        cursor: pointer;\
+    }\
+    .mighty-autos input[type='text'] {width: 100px;}\
+    .mighty-autos select {\
+        background: transparent;\
+        height: 24px;\
+        width: 100%;\
+    }");
 
     return {
 
         // These options will be used as defaults
         options: {
-			ui: {
-				"make": "h3"
-			}
+            ui: {
+                "make": "h3"
+            }
         },
 
-		_create: function () {
-			var self = this;
+        _create: function () {
+            var self = this;
 
-			self._bindEvents();
-		},
+            self._bindEvents();
+        },
 
-		_bindEvents: function () {
+        _bindEvents: function () {
 
-			var self = this,
+            var self = this,
                 elem = self.element,
-                i = 1,
-				form = core.query('div', elem)[0],
-				makeSelect = document.createElement('select'),
-				selectedMake,
-				modelSelects = {},
-				modelSelect,
-				currentMake;
+                i = 0,
+                form = core.query('div', elem)[0],
+                makeSelect = document.createElement('select'),
+                selectedMake,
+                modelSelects = {},
+                modelSelect,
+                currentMake,
 
-			makeSelect.options[0] = new Option('Please Select a Make', '');
-			makeSelect.setAttribute('name', 'make');
-			core.addClass(makeSelect, 'make');
+                that,
+                models;
 
-			while ( form.childNodes[i] ){
-				var that = form.childNodes[i];
+            makeSelect.options[0] = new Option('Please Select a Make', '');
+            makeSelect.setAttribute('name', 'make');
+            core.addClass(makeSelect, 'make');
 
-				if(that.nodeName === 'H3'){
-					makeSelect.appendChild(new Option(that.textContent, that.textContent));
-					currentMake = that.textContent;
-					modelSelects[that.textContent] = that.textContent
-					modelSelect = document.createElement('select');
-					modelSelect.setAttribute('name', 'model');
-					modelSelect.appendChild(new Option('Please Select a Model', ''));
-					modelSelects[currentMake] = modelSelect;
-					that.style.display = 'none';
-				} else if (that.nodeName === 'LABEL') {
-					modelSelect = modelSelects[currentMake];
-					modelSelect.appendChild(new Option(that.textContent, that.textContent));
-					that.style.display = 'none';
-				}
+            while (form.childNodes[i]) {
+                that = form.childNodes[i];
+                if (that.nodeName === 'H3') {
+                    makeSelect.appendChild(new Option(that.innerText, that.innerText));
+                    currentMake = that.innerText;
+                    modelSelect = document.createElement('select');
+                    modelSelect.setAttribute('name', 'model');
+                    modelSelect.appendChild(new Option('Please Select a Model', ''));
+                    modelSelects[currentMake] = modelSelect;
+                    that.style.display = 'none';
+                } else if (that.nodeName === 'LABEL') {
+                    modelSelect = modelSelects[currentMake];
+                    modelSelect.appendChild(new Option(that.innerText, that.innerText));
+                    that.style.display = 'none';
+                }
 
-				i++;
-			}
+                i += 1;
+            }
 
-			form.appendChild(makeSelect);
-			var models = document.createElement('select');
-			models.setAttribute('disabled', 'disabled');
-			models.options[0] = new Option('Please Select a Model', '');
-			form.appendChild(models);
+            form.appendChild(makeSelect);
 
-			core.delegate( elem, 'select.make', 'change', function(event){
-				thisSelect = event.target;
-				selectedMake = thisSelect;
-				modelSelect = core.query('select', elem)[1];
+            models = document.createElement('select');
+            models.setAttribute('disabled', 'disabled');
+            models.options[0] = new Option('Please Select a Model', '');
 
-				if ( selectedMake && selectedMake.value !== '' ) {
-					form.removeChild(modelSelect);
-					form.appendChild(modelSelects[selectedMake.value]);
-					modelSelect.removeAttribute('disabled');
-				}else{
-					modelSelect.setAttribute('disabled', 'disabled');
-				}
+            form.appendChild(models);
 
-			});
+            core.delegate(elem, 'select.make', 'change', function (event) {
 
-		}
+                selectedMake = event.target;
+                modelSelect = core.query('select', elem)[1];
+
+                if (selectedMake && selectedMake.value !== '') {
+                    form.removeChild(modelSelect);
+                    form.appendChild(modelSelects[selectedMake.value]);
+                    modelSelect.removeAttribute('disabled');
+                } else {
+                    modelSelect.setAttribute('disabled', 'disabled');
+                }
+            });
+        }
     };
-
 });
