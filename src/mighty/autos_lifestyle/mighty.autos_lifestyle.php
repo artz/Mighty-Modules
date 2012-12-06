b2b6db790a0b86efe8267e4823d9602aadbf24fd
Andy Kahn <akahn87@gmail.com>
2012-12-06 10:06:58 -0500

    Update src/mighty/autos_lifestyle/mighty.autos_lifestyle.php
    Updating the lifestyle module to fit the updated rewrite rules for choose control

--- src/mighty/autos_lifestyle/mighty.autos_lifestyle.php
+++ src/mighty/autos_lifestyle/mighty.autos_lifestyle.php
@@ -6,7 +6,7 @@ $json = $Mighty->getJSON('http://autos.aol.com/cars-js/global/get_new_data.jsp')
 ?>
 <div class="mighty-autos mighty-autos_lifestyle mighty-reset">
 	<h2 class="header">Find the <em>Right Car</em></h2>
-	<form action="http://autos.aol.com/lifestyle-listings/" enctype="application/x-www-form-urlencoded" method="post">
+	<form action="http://autos.aol.com/choosecontrol-listings/" enctype="application/x-www-form-urlencoded" method="get">
 		<select name="life">
 			<option>Choose Your Lifestyle</option>
 			<option value="single male">Single Male</option>
